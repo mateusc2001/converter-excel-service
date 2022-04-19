38b687f6f72767bfb0358164674e5ca7792c12af
Mateus <mcrodrigues@grupodimed.com.br>
2022-04-19 03:42:04 -0300

    setando configuraçao de cors

--- src/main/java/com/extrato/service/controller/RestController.java
+++ src/main/java/com/extrato/service/controller/RestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
